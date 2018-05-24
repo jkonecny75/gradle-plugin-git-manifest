aa1943d9f485c81f62b54ed686556cb215c9aa96
Jaromir Konecny <jaromir.konecny@ysoft.com>
2018-05-24 16:46:51 +0200

    plugin: Add the task 'prepareSources'

--- src/main/groovy/GitManifest.groovy
+++ src/main/groovy/GitManifest.groovy
@@ -17,6 +17,44 @@ class GitManifestPlugin implements Plugin<Project> {
             println extension.fileName
           }
         }
+       project.task('prepareSources') {
+          group = 'GIT manifest.xml - @YSOFT'
+          description = 'Clone and check out all GIT repositories which constitute this project in "manifest.xml"'
+
+          doLast {
+              def parsedProjectXml = new XmlParser().parse(project.file('manifest.xml'))
+              parsedProjectXml.projects.project.each { p ->
+                  println "Preparing >> source: " + p.@name + ", version : " + p.@revision
+                  def dir = project.file("sources/${p.@path}")
+                  if (!dir.exists()) {
+                      dir.mkdirs()
+                      project.exec {
+                          def baseURL = parsedProjectXml.remotes.remote.find { r -> r.@name == p.@remote }.@fetch
+                          def gitCommand = ['git', 'clone', '-v', '--progress', baseURL + '/' + p.@name, p.@path]
+                          println gitCommand
+                          def stdout = new ByteArrayOutputStream()
+                          workingDir project.file('sources')
+                          commandLine gitCommand
+                          standardOutput = stdout
+                          println stdout.toString().trim()
+                      }
+                  } else {
+                      println "The source ${dir} has already cloned !"
+                  }
+                  project.exec {
+                      def gitCommand = ['git', 'checkout', p.@revision]
+                      println gitCommand
+                      def stdout = new ByteArrayOutputStream()
+                      workingDir dir
+                      commandLine gitCommand
+                      standardOutput = stdout
+                      println stdout.toString().trim()
+                  }
+              }
+          }
+      }
+
+
     }
 }
 
